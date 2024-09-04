de1f20cdd72f9f6438c6dfcadfe16142bf89ff34
Satria Adi Wijaya <143068767+Arthur130719@users.noreply.github.com>
2024-09-05 02:57:05 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function startAnimation() {
-    // Trigger animations here
-    document.getElementById('fireworks').classList.add('fireworks-animation');
-    document.getElementById('balloons').classList.add('balloons-animation');
-    document.getElementById('confetti').classList.add('confetti-animation');
-}
