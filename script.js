10e47882cbdf1b9d99d2d3694bf0604b86d936ca
Satria Adi Wijaya <143068767+Arthur130719@users.noreply.github.com>
2024-09-05 02:46:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function startAnimation() {
+    // Trigger animations here
+    document.getElementById('fireworks').classList.add('fireworks-animation');
+    document.getElementById('balloons').classList.add('balloons-animation');
+    document.getElementById('confetti').classList.add('confetti-animation');
+}
